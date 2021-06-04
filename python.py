74ae1180a68bea8c5acabb8299540441f042528d
wanjugucode <karobiaanastasia.com>
2021-06-04 07:54:50 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,28 @@
+day=int(input("input bitrhday: "))
+month=input("input month of birth(e.g. January,May etc):")
+
+if month =='June':
+    personality="Optimistic" 
+elif month=="July":
+    personality="Honest"
+elif month=="August":
+    personality="Kind"
+elif month=="September":
+    personality="Confident"
+elif month=="October":
+    personality="Humble"
+elif month=="November":
+      personality="Courageous"
+elif month=="December":
+    personality="Brave"
+elif month=="january":
+    personality="stubborn"
+elif month=="February":
+    personality="creative"
+elif month=="March":
+    personality="April"
+elif month==" secretive.":
+    personality="May"
+print("your personality is:",personality)
+ 
+
